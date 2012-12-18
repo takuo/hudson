c54802887401114454c8bc60a8abe0e4874904db
Takuo Kitame <kitame@gmail.com>
2012-12-19 05:16:07 +0900

    Added local setup.sh for cm-10.1

---
+++ cm-10.1-setup.sh
@@ -0,0 +1,13 @@
+#vendor/cm/get-prebuilts
+
+SAVEPWD=`pwd`
+
+PROJECTS="vendor/cm"
+
+for p in $PROJECTS; do
+  repo="android_`echo $p | sed -e 's!/!_!g'`"
+  cd $SAVEPWD/$p && \
+  git fetch http://github.com/takuo/$repo refs/heads/northeye && git merge FETCH_HEAD
+done
+cd $SAVEPWD
+
